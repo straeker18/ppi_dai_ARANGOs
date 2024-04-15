d2f6b0f4a87ebf3da5b1396fba1f54ba955d2260
Santiago Arango Villa <49077085+straeker18@users.noreply.github.com>
2024-04-15 12:08:23 -0500

    Add files via upload
    web site with flask

---
+++ AI/log/app.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask import render_template, request, redirect, Response, url_for, session
+from flask_mysqldb import MySQL,MySQLdb # pip install Flask-MySQLdb
+
+app = Flask(__name__,template_folder='template')
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'usuario'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')   
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')   
+
+@app.route('/acceso-login', methods= ["GET", "POST"])
+def login():
+   
+    if request.method == 'POST' and 'txtCorreo' in request.form and 'txtPassword' in request.form:
+       
+        _correo = request.form['txtCorreo']
+        _password = request.form['txtPassword']
+
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM usuarios WHERE correo = %s AND password = %s', (_correo, _password,))
+        account = cur.fetchone()
+      
+        if account:
+            session['logueado'] = True
+            session['id'] = account['id']
+
+            return render_template("admin.html")
+        else:
+            return render_template('index.html',mensaje="Usuario O Contraseña Incorrectas")
+
+    
+if __name__ == '__main__':
+   app.secret_key = "pinchellave"
+   app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)

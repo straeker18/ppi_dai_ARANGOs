c94d16ac3909c45b706d119b187e2dd3a9982e10
Santiago Arango Villa <49077085+straeker18@users.noreply.github.com>
2024-08-03 20:00:02 -0500

    Add files via upload

--- log/app.py
+++ log/app.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from flask import render_template, request, redirect, Response, url_for, session
-from flask_mysqldb import MySQL,MySQLdb # pip install Flask-MySQLdb
+import mysql.connector
 
 app = Flask(__name__,template_folder='template')
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'usuario'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-mysql = MySQL(app)
+def get_db_connection():
+    conn = mysql.connector.connect(
+        host='localhost',
+        user='yourusername',
+        password='Straeker_2002',
+        database='yourdatabase'
+    )
+    return conn
 
 @app.route('/')
 def home():
